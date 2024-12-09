d47746672578bb802daa0cdd871226747ece0de0
Omodamola Oladeji <darmhoo@hotmail.com>
2024-12-09 16:27:26 +0000

    fetchNews

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,23 @@ class Article extends Model
 {
     //
     protected $guarded = [];
+
+    public function scopeFilter($query)
+    {
+        if (request('category')) {
+            $query->where('category', request('category'));
+        }
+
+        if (request('source')) {
+            $query->where('source', request('source'));
+        }
+
+        if (request('date')) {
+            $query->where('publishedAt', request('date'));
+        }
+
+        if (request('keyword')) {
+            $query->where('title', 'like', '%' . request('keyword') . '%');
+        }
+    }
 }
